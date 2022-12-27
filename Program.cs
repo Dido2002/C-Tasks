7b0df6bedb3eab19260e07e5164841e9b7e3a239
Dido <100509456+Dido2002@users.noreply.github.com>
2022-12-27 16:15:29 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,23 @@
 ﻿using System;
-using System.Linq;
 
-namespace Array03
+namespace Arrays
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			string[] rawInput = Console.ReadLine().Split();
-			double[] items = new double[rawInput.Length];
+			string[] daysOfTheWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+			int day = int.Parse(Console.ReadLine());
 
-			for (int i = 0; i < rawInput.Length; i++)
+			if (day >= 1 && day <= 7)
 			{
-				items[i] = double.Parse(rawInput[i]);
+				//THis is how to take the current day we take 1 because the array start with number 0
+				Console.WriteLine($"The day of the week is: { daysOfTheWeek[day - 1]}");
+			}
+			else {
+				Console.WriteLine("You got something wrong!Try again");
 			}
 
-			for (int i = 0; i < items.Length; i++)
-			{
-				Console.WriteLine($"{items[i]} => {Math.Round(items[i], MidpointRounding.AwayFromZero)}");
-			}
-			
 		}
-
 	}
 }
